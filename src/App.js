8b8ef751194690b1e995d9e5d8e4446627aa14b6
ChrisBrownTheGuy <ChrisBrownTheGuy@users.noreply.github.com>
2020-05-30 10:32:25 -0500

    Finally able to display all the characters. Had to use a nested for loop then push the results to a let vari then before I could use it in state vari

--- src/App.js
+++ src/App.js
@@ -14,19 +14,34 @@ class App extends Component {
   }
 
   componentDidMount() {
+    let _starPeople = [];
     for (let i = 1; i <= 9; i++) {
       fetch(`https://swapi.dev/api/people/?page=${i}`)
         .then(resp => resp.json())
         .then(result => {
-          // console.log(results);
-          return this.setState({ starPeople: result.results });
+          // console.log('page: ', i, result.results);
+          // return this.setState({ starPeople: });
+          // console.log(typeof this.starPeople);
+          for (let j = 0; j < result.results.length; j++) {
+            // console.log(_starPeople);
+            console.log(result.results[j]);
+            _starPeople.push(result.results[j]);
+          }
+          console.log(_starPeople.length);
+
+          // return this.setState({ starPeople: result.results });
+        })
+        .finally(_ => {
+          return this.setState({ starPeople: _starPeople });
         });
     }
-    // for (let i = 1; i <= 9; i++) {
-    //   // let newSult;
-    //   fetch(`https://swapi.dev/api/people/?page=${i}`)
+    // return this.setState({ starPeople: _starPeople });
+
+    // for (let i = 1; i <= 82; i++) {
+    //   fetch(`https://swapi.dev/api/people/${i}/`)
     //     .then(resp => resp.json())
     //     .then(result => {
+    //       console.log(result.results);
     //       return this.setState({ starPeople: result.results });
     //     });
     // }
@@ -49,10 +64,23 @@ class App extends Component {
   render() {
     const { starPeople, searchfield } = this.state;
     const filteredStar = starPeople.filter(starPerson => {
-      console.log(starPerson.name);
+      // console.log(starPerson.name);
       return starPerson.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    // console.log(filteredStar);
+
+    // const filteredStar = starPeople
+    //   .map(starGroup => {
+    //     const stars = starGroup;
+    //     // console.log(stars);
+    //     return stars;
+    //   })
+    //   .filter(starPerson => {
+    //     console.log(starPerson.name);
+    //     return starPerson.name
+    //       .toLowerCase()
+    //       .includes(searchfield.toLowerCase());
+    //   });
+
     return (
       <div className="tc">
         <h1 className="f1">Star Wars Universe</h1>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-
 const SearchBox = ({ searchfield, searchChange }) => {
-
-    return(
-        <div className='pa2'>
-            <input
-              className='pa3 ba b--blue bg-lightest-blue' 
-              type='search' 
-              placeholder='Search Universe' 
-              onChange={searchChange}            
-              />
-        </div>
-    );
-
-}
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--blue bg-lightest-blue"
+        type="search"
+        placeholder="Search Universe"
+        onChange={(searchfield, searchChange)}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
