42af11068d5b6edca1d12a7b5b65d5ce488653bc
ChrisBrownTheGuy <ChrisBrownTheGuy@users.noreply.github.com>
2020-05-31 14:21:53 -0500

    revised aysnc await

--- src/App.js
+++ src/App.js
@@ -13,50 +13,74 @@ class App extends Component {
     };
   }
 
+  // componentDidMount() {
+  //   // let _starPeople = [];
+  //   // for (let i = 1; i <= 9; i++) {
+  //   //   fetch(`https://swapi.dev/api/people/?page=${i}`)
+  //   //     .then(resp => resp.json())
+  //   //     .then(result => {
+
+  //   //       for (let j = 0; j < result.results.length; j++) {
+  //   //         // console.log(_starPeople);
+  //   //         console.log(result.results[j]);
+  //   //         _starPeople.push(result.results[j]);
+  //   //       }
+  //   //       console.log(_starPeople.length);
+
+  //   //       // return this.setState({ starPeople: result.results });
+  //   //     })
+  //   //     .finally(_ => {
+  //   //       return this.setState({ starPeople: _starPeople });
+  //   //     });
+  //   // }
+
+  //   // <--------------------------
+
   componentDidMount() {
     let _starPeople = [];
-    for (let i = 1; i <= 9; i++) {
-      fetch(`https://swapi.dev/api/people/?page=${i}`)
-        .then(resp => resp.json())
-        .then(result => {
-          // console.log('page: ', i, result.results);
-          // return this.setState({ starPeople: });
-          // console.log(typeof this.starPeople);
-          // for loop
-          for (let j = 0; j < result.results.length; j++) {
-            // console.log(_starPeople);
-            console.log(result.results[j]);
-            _starPeople.push(result.results[j]);
+
+    let i = 1;
+
+    let fetchStarPeople = async index => {
+      await fetch(`https://swapi.dev/api/people/?page=${index}`)
+        .then(response => {
+          if (response.ok) {
+            index++;
+            let data = response.json();
+
+            return data;
+          } else {
+            throw new Error(response.status);
           }
-          console.log(_starPeople.length);
-
-          // return this.setState({ starPeople: result.results });
         })
-        .finally(_ => {
-          return this.setState({ starPeople: _starPeople });
+        .then(data => {
+          for (let j = 0; j < data.results.length; j++) {
+            _starPeople.push(data.results[j]);
+          }
+          console.log(_starPeople);
+          this.setState({ starPeople: _starPeople });
+          fetchStarPeople(index);
+        })
+        .catch(error => {
+          if (error.message === 404) {
+            console.log('There are no more star people to load!');
+          } else {
+            console.log('error ', error);
+          }
+          console.log(
+            `You just pulled down ${_starPeople.length} star people!`
+          );
         });
-    }
-    // return this.setState({ starPeople: _starPeople });
+    };
 
-    // for (let i = 1; i <= 82; i++) {
-    //   fetch(`https://swapi.dev/api/people/${i}/`)
-    //     .then(resp => resp.json())
-    //     .then(result => {
-    //       console.log(result.results);
-    //       return this.setState({ starPeople: result.results });
-    //     });
-    // }
+    let makeCall = async () => {
+      await fetchStarPeople(i).finally(_ => {
+        console.log('testing');
+      });
+    };
+
+    makeCall();
   }
-
-  //
-  //   async function getStar() {
-  //     const resp = await fetch(`https://swapi.dev/api/people/${i}/`);
-  //     const respData = await resp.json();
-  //     console.log(respData);
-  //     return respData;
-  //   }
-  //
-  //
 
   onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
@@ -68,19 +92,6 @@ class App extends Component {
       // console.log(starPerson.name);
       return starPerson.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-
-    // const filteredStar = starPeople
-    //   .map(starGroup => {
-    //     const stars = starGroup;
-    //     // console.log(stars);
-    //     return stars;
-    //   })
-    //   .filter(starPerson => {
-    //     console.log(starPerson.name);
-    //     return starPerson.name
-    //       .toLowerCase()
-    //       .includes(searchfield.toLowerCase());
-    //   });
 
     return (
       <div className="tc">
